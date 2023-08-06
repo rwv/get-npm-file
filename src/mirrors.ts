2dc14b8c7595de1524985a29befd2e743b0fae6c
Seedgou <i@zczc.cz>
2023-08-06 20:38:47 +0800

    add npm mirror

--- src/mirrors.ts
+++ src/mirrors.ts
@@ -4,8 +4,20 @@ export const globalMirrors: Mirror[] = ['https://cdn.jsdelivr.net/npm']
 
 export const CNMirrors: Mirror[] = [
   'https://npm.elemecdn.com',
+  npmmirror,
   'https://jsd.cdn.zzko.cn/npm',
   'https://jsd.onmicrosoft.cn/npm',
   'https://jsdelivr.codeqihan.com/npm',
   'https://jsdelivr.b-cdn.net/npm'
 ]
+
+// npmmirror
+// https://github.com/cnpm/cnpmcore/issues/452
+// https://www.zhihu.com/question/62938096
+function npmmirror (name: string, version: string | undefined, path: string): string {
+  if (version !== undefined) {
+    return `https://registry.npmmirror.com/${name}/${version}/files/${path}`
+  } else {
+    return `https://registry.npmmirror.com/${name}/latest/files/${path}`
+  }
+}

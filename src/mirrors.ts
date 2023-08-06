20dc84ed5d400c3ad0065c702c4d1899b670e421
Seedgou <i@zczc.cz>
2023-08-06 20:03:26 +0800

    add mirrors

---
+++ src/mirrors.ts
@@ -0,0 +1,9 @@
+export const globalMirrors = ['https://cdn.jsdelivr.net/npm'] as const
+
+export const CNMirrors = [
+  'https://npm.elemecdn.com',
+  'https://jsd.cdn.zzko.cn/npm',
+  'https://jsd.onmicrosoft.cn/npm',
+  'https://jsdelivr.codeqihan.com/npm',
+  'https://jsdelivr.b-cdn.net/npm'
+]

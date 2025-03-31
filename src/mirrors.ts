cb9cd49bb751d86a0eed6ccbea7aeb9f4891c34f
Seedgou <i@zczc.cz>
2025-03-31 16:38:56 +0800

    fix(mirrors): update CNMirrors to replace unavailable URLs with a new CDN

--- src/mirrors.ts
+++ src/mirrors.ts
@@ -3,9 +3,7 @@ export type Mirror = string | ((name: string, version: string | undefined, path:
 export const globalMirrors: Mirror[] = ['https://cdn.jsdelivr.net/npm']
 
 export const CNMirrors: Mirror[] = [
-  'https://npm.elemecdn.com',
   'https://jsd.cdn.zzko.cn/npm',
   'https://jsd.onmicrosoft.cn/npm',
-  'https://jsdelivr.codeqihan.com/npm',
-  'https://jsdelivr.b-cdn.net/npm'
+  'https://cdn.jsdmirror.com/npm'
 ]
